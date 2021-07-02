c281ea44da209aa7275e214d8135687f506450b8
apm29 <835170486@qq.com>
2021-07-02 09:30:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+ <div>
+   <Dialogs/>
+   <button @click="dialog.alert('onClickonClickonClickonClickonClickonClickonClickonClickonClick')">dialog</button>
+   <button @click="dialog.confirm('1')">dialog</button>
+ </div>
+</template>
+
+<script>
+import { dialog } from "./components/dialog.js"
+import Dialogs from './components/Dialogs.vue'
+export default {
+  components: {Dialogs},
+  data(){
+    return {
+      dialog
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Dialogs from './Dialogs'
+import { dialog } from './dialog'
+
+export {
+  Dialogs,dialog
+}
