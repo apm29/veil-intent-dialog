35040508065c86e1ed1dec560a105ec91f03ff9c
apm29 <835170486@qq.com>
2021-07-05 16:43:33 +0800

    git:ignore

---
+++ dev/serve.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <VeilIntentDialog/>
+    <button @click="dialog.alert('onClickonClickonClickonClickonCliXXXXXXXckonClickonClickonClickonClick')">dialog</button>
+    <button @click="dialog.confirm('1')">dialog</button>
+  </div>
+</template>
+
+<script>
+import { VeilIntentDialog,dialog } from '@/entry.esm'
+export default {
+  components: { VeilIntentDialog },
+  data(){
+    return {
+      dialog
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/lib-components/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+import "./index.css"
+export { default as VeilIntentDialog } from './Dialogs.vue';
+export { dialog as dialog } from './dialog';
