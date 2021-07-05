4d80d543011b885612c5a64ee37f29f112980f53
apm29 <835170486@qq.com>
2021-07-05 17:01:26 +0800

    refactor:使用veil-transitions

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 import "./index.css"
+import "veil-transitions/src/lib-components/index.css"
 export { default as VeilIntentDialog } from './Dialogs.vue';
 export { dialog as dialog } from './dialog';
